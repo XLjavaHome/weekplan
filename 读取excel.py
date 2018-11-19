a1b6838f201a141648ff9e8dbcd0910bcae00039
xuli <78645574@qq.com>
2018-11-19 09:57:56 +0800

    优化周报

--- 读取excel.py
+++ 读取excel.py
@@ -8,19 +8,21 @@ import util.文件
 class weekcontent(object):
     pass
 def get_week(diretory):
-    # 上周计划
+    # 上周工作内容
+    # 上周的任务
     lastTaskSet = set()
-    # 类型
-    lastbugSet = set()
+    # 上周的BUG需求
+    lastbugRequirementSet = set()
+    # 上周修复的BUG
+    lastWeekBug = set()
     # 本周计划
+    # 本周的工作任务
     taskSet = set()
+    # 本周BUG的需求
+    bugRequirementSet = set()
+    # 本周的BUG
     bugSet = set()
-    # 上周修复BUG数量
-    lastWeekBugCount = set()
     # 上周开发任务数
-    lastWeekTaskCount = 0;
-    # 本周的BUG数量
-    bugSetCount = set()
     for file in util.文件.get_excel(diretory):
         excelFile = xlrd.open_workbook(file)
         # 获取第一个sheet，第一行是日志类型就进行。
@@ -33,20 +35,22 @@ def get_week(diretory):
             for row in range(0, maxrow):
                 # 第一列的内容
                 cellType = sheet.cell(row, 0)
+                # 上周的需求名称
                 textValue = sheet.cell(row, 5).value
                 if cellType.value == 'BUG修复' and textValue != '':
-                    lastWeekBugCount.add(sheet.cell(row, 4).value)
-                    lastbugSet.add(textValue)
+                    lastWeekBug.add(sheet.cell(row, 4).value)
+                    if (textValue != ""):
+                        lastbugRequirementSet.add(textValue)
                 if cellType.value == '开发任务' and textValue != '':
-                    lastWeekTaskCount += 1
                     lastTaskSet.add(textValue)
         # 本周工作计划,从第一行开始
         if cell0.value == 'BUG编号':
             for row in range(1, maxrow):
-                # 需求名称
-                bugSet.add(sheet.cell(row, 1).value)
-                # BUG数量
-                bugSetCount.add(sheet.cell(row, 0).value)
+                # 需求名称,需求是有可能没值的
+                if (sheet.cell(row, 1).value != ""):
+                    bugRequirementSet.add(sheet.cell(row, 1).value)
+                # BUG
+                bugSet.add(sheet.cell(row, 0).value)
                 # 本周工作计划
         if cell0.value == '任务名称':
             for row in range(1, maxrow):
@@ -57,15 +61,15 @@ def get_week(diretory):
     if lastTaskSet:
         result += "{0}.开发{1}的任务。\n".format(*[num, '、'.join(lastTaskSet)])
         num = num + 1
-    if lastbugSet:
-        result += "{0}.修复{1}的BUG。（{2}个BUG）\n".format(*[num, '、'.join(lastbugSet), len(lastWeekBugCount)])
+    if lastbugRequirementSet:
+        result += "{0}.修复{1}的BUG。（{2}个BUG）\n".format(*[num, '、'.join(lastbugRequirementSet), len(lastWeekBug)])
     result += '本周工作计划：\n'
     num = 1
     if taskSet:
         result += "{0}.开发{1}任务。\n".format(*[num, '、'.join(taskSet)])
         num = num + 1
     if bugSet:
-        result += "{0}.修复{1}的BUG。（{2}个BUG）\n".format(*[num, '、'.join(bugSet), len(bugSetCount)])
+        result += "{0}.修复{1}的BUG。（{2}个BUG）\n".format(*[num, '、'.join(bugRequirementSet), len(bugSet)])
     return result
 if __name__ == '__main__':
     # 扫描的文件目录
